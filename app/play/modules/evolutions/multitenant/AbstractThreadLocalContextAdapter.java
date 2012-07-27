150026a854fae8a4b69269d8b35689142bfd39b6
Emmanuel FONTAN <emmanuel.fontan@viareport.com>
2012-07-27 16:02:26 +0200

    Rectification sur changements contexte

--- app/play/modules/evolutions/multitenant/AbstractThreadLocalContextAdapter.java
+++ app/play/modules/evolutions/multitenant/AbstractThreadLocalContextAdapter.java
@@ -1,16 +1,13 @@
 package play.modules.evolutions.multitenant;
 
-import multitenant.context.AbstractThreadLocalContext;
-import multitenant.context.MultiTenantContext;
 
-public class AbstractThreadLocalContextAdapter extends
-        AbstractThreadLocalContext implements ContextAdapterInterface {
+public class AbstractThreadLocalContextAdapter implements ContextAdapterInterface {
 
     public void setClientCode(String clientCode) {
-        ((StaticContext) MultiTenantContext.current()).setClientCode(clientCode);
+//        ((StaticContext) MultiTenantContext.current()).setClientCode(clientCode);
     }
-    
+
     public void setUserLogin(String userLogin) {
-        ((StaticContext) MultiTenantContext.current()).setUserLogin(userLogin);
+//        ((StaticContext) MultiTenantContext.current()).setUserLogin(userLogin);
     }
 }
